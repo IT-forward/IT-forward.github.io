d71b27ba45be9688f94576e7d571410279d75532
Husan-Eshonqulov <husan.eshonqulov.00@gmail.com>
2021-05-11 18:41:22 +0500

    cannot find react

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -1,25 +1,86 @@
+import React from 'react';
+import '../LandingPage/LandingPage.css';
+import headerImg from './LandingPageImages/header-image.svg';
+import featuresImg from './LandingPageImages/features-img.svg';
+// import { AiFillCheckCircle } from 'react-icons/ai';
+
+// type HTMLElement = 
+
 function LandingPage() {
     return (
-        <div>
-            <header>
-                <nav>
-                    <ul className="nav justify-content-end">
-                        <li className="nav-item">
-                            <a className="nav-link active" href="#">Active</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Disabled</a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+        <div></div>
+        // <div className="" id="body">
+        //     <div id="header">
+        //         <div className="d-flex justify-content-between" id="navbar">
+        //             <div className="d-flex align-items-center" id="brand-logo">
+        //                 Pay IT Forward
+        //             </div>
+        //             <ul className="nav justify-content-end" id="nav-links">
+        //                 <li className="nav-item">
+        //                     <a className="nav-link" id="nav-anchor-1" href="#">Navigation 1</a>
+        //                 </li>
+        //                 <li className="nav-item">
+        //                     <a className="nav-link" id="nav-anchor-2" href="#">Navigation 2</a>
+        //                 </li>
+        //                 <li className="nav-item">
+        //                     <a className="nav-link" id="nav-anchor-3" href="#">Navigation 3</a>
+        //                 </li>
+        //                 <li className="nav-item">
+        //                     <a className="nav-link" id="nav-anchor-4" href="#">Navigation 4</a>
+        //                 </li>
+        //             </ul>
+        //         </div>
+
+        //         <div id="description">
+        //             <div className="row">
+        //                 <div className="col-6" id='desc-text'>
+        //                     <div>
+        //                         <h1 id="desc-heading">
+        //                             Lorem ipsum dolor sit consectetur
+        //                         </h1>
+        //                     </div>
+
+        //                     <div>
+        //                         <p id="desc-paragraph">
+        //                             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        //                             Impedit laudantium enim, quidem odio qui architecto suscipit
+        //                         </p>
+        //                     </div>
+        //                 </div>
+
+        //                 <div className="col-6" id="desc-img">
+        //                     <img src={headerImg} alt="header image" />
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+
+        //     <div id="features">
+        //         <div className="row">
+        //             <div className="col-6" id="features-img">
+        //                 <img src={featuresImg} alt="features image" />
+        //             </div>
+
+        //             <div className="col-6" id="features-text">
+        //                 <h2>
+        //                     We Provide Many Features You Can Use
+        //                 </h2>
+
+        //                 <p>
+        //                     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        //                     At quia nihil dolorem perferendis ab ad.
+        //                 </p>
+
+        //                 <ul className="list-unstyled">
+        //                     {/* <li><AiFillCheckCircle />feature 1</li> */}
+        //                     <li>feature 2</li>
+        //                     <li>feature 3</li>
+        //                     <li>feature 4</li>
+        //                 </ul>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
 
