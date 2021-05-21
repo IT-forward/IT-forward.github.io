c51ee9e92ccad49a2dd65c5e472d9b6ff4cc6011
Husan-Eshonqulov <husan.eshonqulov.00@gmail.com>
2021-05-21 16:45:50 +0500

    without contact us; responsive

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -1,10 +1,9 @@
 import '../LandingPage/LandingPage.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import brandLogo from './LandingPageImages/brand-logo.png';
 import headerImg from './LandingPageImages/header-image.svg';
 import featuresImg from './LandingPageImages/features-img.svg';
 import { AiFillCheckCircle } from 'react-icons/ai';
-import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import { Container, Nav, Navbar, Row, Col } from "react-bootstrap";
 
 function LandingPage() {
     return (
@@ -24,36 +23,36 @@ function LandingPage() {
                     </Navbar.Collapse>
                 </Navbar>
 
-                <article id="description" className="mt-5">
-                    <Row xs={1} sm={2}>
-                        <Col className="mt-sm-4">
-                            <h1 id="desc-heading" className="mb-4">Lorem ipsum dolor sit amet consectetur</h1>
-                            <p id="desc-paragraph">
+                <article id="description">
+                    <Row xs={1} sm={1} md={2}>
+                        <Col>
+                            <h1 id="desc-heading">Lorem ipsum dolor sit amet consectetur</h1>
+                            <p id="desc-paragraph" className="mt-md-2 mb-md-4 mt-lg-4 mb-lg-5">
                                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt non explicabo dolorum molestias
                             </p>
-                            <div id="btn-con" className="mt-5">
+                            <div>
                                 <button className="btn btn-lg btn-danger border-0" id="desc-btn">Get Started</button>
                             </div>
                         </Col>
 
-                        <Col id="desc-image" className="mt-sm-4">
+                        <Col id="desc-image">
                             <img src={headerImg} className="img-fluid" alt="description" />
                         </Col>
                     </Row>
                 </article>
             </header>
 
-            <section id="features" className="mt-5">
-                <Row xs={1} sm={2}>
-                    <Col id="feature-image" className="mt-sm-5">
+            <section id="features">
+                <Row xs={1} sm={1} md={2}>
+                    <Col id="feature-image">
                         <img src={featuresImg} className="img-fluid" alt="feature image" />
                     </Col>
 
-                    <Col className="mt-sm-5 pl-sm-5 d-sm-flex align-items-end">
+                    <Col className="pl-md-5 d-md-flex align-items-end">
                         <Row>
                             <Col id="features-desc">
                                 <h2>We Provide Many Features You Can Use</h2>
-                                <p className="my-3">
+                                <p className="my-md-2 my-lg-3">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nisi pariatur, animi facilis quas eius quia fuga totam!
                                 </p>
                                 <ul className="list-unstyled mb-0">
@@ -68,78 +67,30 @@ function LandingPage() {
                 </Row>
             </section>
 
-            <footer className="mt-5 mb-4">
+            <footer>
                 <Row>
-                    <Col sm={12} md={5} lg={6}>
+                    <Col sm={12} md={3} lg={4} className="d-flex align-items-center justify-content-center justify-content-md-start">
                         <div id="footer-cap" className="font-weight-bolder">Open Enterprise</div>
                     </Col>
 
-                    <Col sm={12} md={7} lg={6}>
-                        <Row sm={1} md={4} className="justify-content-end">
-                            <Col id="footer-link" md="auto" className="mr-md-3 px-0">
+                    <Col sm={12} md={9} lg={8}>
+                        <Row sm={1} md={4} className="justify-content-end text-center">
+                            <Col md="auto" id="footer-link" className="ml-md-3 ml-lg-5">
                                 <a href="#" id="footer-link-1" className="text-decoration-none">Docs</a>
                             </Col>
-                            <Col id="footer-link" md="auto" className="mr-md-3 px-0">
+                            <Col md="auto" id="footer-link" className="ml-md-3 ml-lg-5">
                                 <a href="#" id="footer-link-2" className="text-decoration-none">GitHub</a>
                             </Col>
-                            <Col id="footer-link" md="auto" className="mr-md-3 px-0">
+                            <Col md="auto" id="footer-link" className="ml-md-3 ml-lg-5">
                                 <a href="#" id="footer-link-3" className="text-decoration-none">Twitter</a>
                             </Col>
-                            <Col id="footer-link" md="auto" className="px-0">
+                            <Col md="auto" id="footer-link" className="ml-md-3 ml-lg-5">
                                 <a href="#" id="footer-link-4" className="text-decoration-none">Contact us</a>
                             </Col>
                         </Row>
-
                     </Col>
                 </Row>
             </footer>
-
-            {/* <div className="row align-items-center">
-                <div className="col-md-6">
-                    <div id="footer-cap" className="font-weight-bolder">Open Enterprise</div>
-                </div>
-
-                <div className="col-md-6">
-                    <div className="row">
-                        <div className="col-xl-3 col-md-6 mt-3" id="footer-link">
-                            <a
-                                href="#"
-                                id="footer-link-1"
-                                className="text-decoration-none font-weight-medium"
-                            >
-                                Docs
-                                    </a>
-                        </div>
-                        <div className="col-xl-3 col-md-6 mt-3" id="footer-link">
-                            <a
-                                href="#"
-                                id="footer-link-2"
-                                className="text-decoration-none font-weight-medium"
-                            >
-                                GitHub
-                                    </a>
-                        </div>
-                        <div className="col-xl-3 col-md-6 mt-3" id="footer-link">
-                            <a
-                                href="#"
-                                id="footer-link-3"
-                                className="text-decoration-none font-weight-medium"
-                            >
-                                Twitter
-                                    </a>
-                        </div>
-                        <div className="col-xl-3 col-md-6 mt-3" id="footer-link">
-                            <a
-                                href="#"
-                                id="footer-link-4"
-                                className="text-decoration-none font-weight-medium"
-                            >
-                                Contact us
-                                    </a>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
         </Container>
     );
 }
