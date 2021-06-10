6dbe1772ca5f207f688c4e8a1d8fc568d91925ba
Husan-Eshonqulov <husan.eshonqulov.00@gmail.com>
2021-06-10 08:40:21 +0500

    some changes

--- src/memberAccounts/projects/Project.tsx
+++ src/memberAccounts/projects/Project.tsx
@@ -3,12 +3,14 @@ import './Project.css';
 
 function Project() {
     return (
-        <a href="https://light-switch-00.netlify.app/" target="_blank" className="project">
-            <div><img src={project1Img} alt="project 1" className="img-fluid" /></div>
-            <div className="project-title-div">
-                <div className="project-title text-center">Light Switch</div>
-            </div>
-        </a>
+        <div className="project">
+            <a href="https://light-switch-00.netlify.app/" target="_blank" className="anchor">
+                <div><img src={project1Img} alt="project 1" className="img-fluid" /></div>
+                <div className="project-title-div">
+                    <div className="project-title text-center">Light Switch</div>
+                </div>
+            </a>
+        </div>
     );
 }
 
