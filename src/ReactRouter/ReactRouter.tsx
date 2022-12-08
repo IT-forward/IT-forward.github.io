74295e4ed744b7eb00bd419fb15ad59fe6566e84
bahriddin <bahriddin.abdiev@ferocia.com.au>
2022-12-08 22:52:07 +1100

    A'zo profili sahifasi Github Pages'da ishlashi kerak

--- src/ReactRouter/ReactRouter.tsx
+++ src/ReactRouter/ReactRouter.tsx
@@ -1,30 +1,24 @@
-
-import Landingpage from '../LandingPage/LandingPage';
-import Roadmap from '../Roadmap/Roadmap';
-import orgChart from '../orgChart/OrgChartTree';
-import Introduction from '../LandingPage/Introduction';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import LessonPage from '../Roadmap/components/LessonPage';
-import Screen from '../memberAccounts/Screen';
-
+import Landingpage from "../LandingPage/LandingPage";
+import Roadmap from "../Roadmap/Roadmap";
+import orgChart from "../orgChart/OrgChartTree";
+import Introduction from "../LandingPage/Introduction";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import LessonPage from "../Roadmap/components/LessonPage";
+import Screen from "../memberAccounts/Screen";
 
 function ReactRouter() {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route path="/" exact component={Landingpage}/>
-                    <Route path="/roadmap"   component={Roadmap} />
-                    <Route path="/organization-tree"  component={orgChart} />
-                    <Route path="/introduction"  component={Introduction} />
-                    <Route path="/lessons/:id"  component={LessonPage} />
-                    <Route path="/member-account/:id" component={Screen} />
-                </Switch>
-            </div>
-        </Router>
-    )
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/" exact component={Landingpage} />
+        <Route path="/roadmap" component={Roadmap} />
+        <Route path="/organization-tree" component={orgChart} />
+        <Route path="/introduction" component={Introduction} />
+        <Route path="/lessons/:id" component={LessonPage} />
+        <Route path="/member-account/:id" component={Screen} />
+      </Switch>
+    </HashRouter>
+  );
 }
-
-
 
 export default ReactRouter;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from "react-dom";
 //import LandingPage from "./LandingPage/LandingPage";
 import "bootstrap/dist/css/bootstrap.css";
-import ReactRouter from './ReactRouter/ReactRouter';
+import ReactRouter from "./ReactRouter/ReactRouter";
+import React from "react";
 
-ReactDOM.render(<ReactRouter />, document.getElementById("root"));
-
+ReactDOM.render(
+  <React.StrictMode>
+    <ReactRouter />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
